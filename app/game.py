cdcde45f62f906be73e36d5a1feb3d1790cf4215
Rebecca Fernandez <97413846+beckyfernez@users.noreply.github.com>
2022-03-28 15:05:51 -0400

    Game improvements (#1)
    here is an explanation on what this PR is about and the changes
    
    * Refactoring - simplify valid options
    
    Created a variable for a duplicated list so that there is only one place needed for updates in the future.
    
    * Add code climate config file

--- app/game.py
+++ app/game.py
@@ -4,13 +4,16 @@
 
 from random import choice
 
+#single place for updates in the future
+valid_selections = ["rock", "paper", "scissors"]
+
 #
 # USER SELECTION
 #
 
 u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
 print("USER CHOICE:", u)
-if u not in ["rock", "paper", "scissors"]:
+if u not in valid_selections:
     print("OOPS, TRY AGAIN")
     exit()
 
@@ -18,7 +21,7 @@ if u not in ["rock", "paper", "scissors"]:
 # COMPUTER SELECTION
 #
 
-c = choice(["rock", "paper", "scissors"])
+c = choice(valid_selections)
 print("COMPUTER CHOICE:", c)
 
 #
